85357fdd73655fd673a5cd5af53ae1f6a6000fee
zedworkspace <shanoof.001@gmail.com>
2025-02-18 17:21:00 +0530

    feat(toast):add toast using shadcn toast

--- src/components/leftSection/leftSection.tsx
+++ src/components/leftSection/leftSection.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Plus } from "lucide-react";
+import { useNewProjectStore } from "@/store/projectStore";
 
 export default function LeftSection() {
+  const { onOpen } = useNewProjectStore();
   return (
     <div className="p-2 space-y-2">
       <Avatar>
         <AvatarImage src="" />
         <AvatarFallback>P</AvatarFallback>
       </Avatar>
-      <Button size="icon" className="border-non rounded-full">
+      <Button size="icon" className="border-non rounded-full" onClick={onOpen}>
         <Plus />
       </Button>
     </div>
--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border-none bg-secondary text-muted-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
