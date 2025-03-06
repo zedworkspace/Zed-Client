19565f75d6d77868dc15fdacb557450aaa53823e
zedworkspace <shanoof.001@gmail.com>
2025-03-06 21:12:46 +0530

    changes in log 1

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -4,6 +4,10 @@ import BoardContents from "./boardContents";
 import { useParams } from "next/navigation";
 import { DndContext, DragEndEvent } from "@dnd-kit/core";
 import { IList } from "@/interface/listInterface";
+import { useGetBoardById } from "@/hooks/useBoard";
+import { useGetLists } from "@/hooks/useList";
+import { useQueryClient } from "@tanstack/react-query";
+import { useBoardSocket } from "@/context/boardSocketProvider";
 
 type Props = {
   boardId: string;
@@ -50,25 +54,12 @@ export default function Board({}: Props) {
 
     if (!sourceList || !targerList || sourceList === targerList) return;
 
-    // const list = lists?.find((list) => list._id === active.data.current?.list);
-    // console.log("activeList", list);
-    // const card = list?.cards.find((card) => card._id === active.id);
-    // console.log("activeCard", card);
-    // const result = lists?.map((list) =>
-    //   over.id === list._id ? list?.cards.push(card!) : list
-    // );
-    // console.log("result", result);
-    // const final = lists?.map((list) =>
-    //   list._id === active.data.current?.list
-    //     ? list.cards.map((card) => card._id !== active.id)
-    //     : list
-    // );
-    // console.log("final ", final);
+    
   };
 
   return (
     <div className="h-full flex flex-col">
-      <BoardHeader board={board} />
+      <BoardHeader board={boardLists?.data} />
       <DndContext onDragEnd={handleDragEnd}>
         <BoardContents lists={lists} boardId={channelId} />
       </DndContext>
