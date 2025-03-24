0726537568e89e133b33d1e5001198225bd75d4e
zedworkspace <shanoof.001@gmail.com>
2025-03-24 13:57:44 +0530

    feat(list):implement list deletion

--- src/components/modals/createChannelModal.tsx
+++ src/components/modals/createChannelModal.tsx
@@ -104,8 +104,8 @@ export function CreateChannel() {
               >
                 {/* Text Channel */}
                 <label
-                  className={`flex items-center justify-between  h-16 rounded-md px-4 cursor-pointer hover:bg-secondary transition ${
-                    type === "text" ? "bg-secondary" : "bg-primary"
+                  className={`flex items-center justify-between  h-16 rounded-md px-4 cursor-pointer hover:bg-gray-700 transition ${
+                    type === "text" ? " bg-indigo-500" : "bg-secondary"
                   }`}
                 >
                   <div className="flex items-center gap-4">
@@ -122,8 +122,8 @@ export function CreateChannel() {
 
                 {/* Voice Channel */}
                 <label
-                  className={`flex items-center justify-between  h-16 rounded-md px-4 cursor-pointer hover:bg-secondary transition ${
-                    type === "voice" ? "bg-secondary/90" : "bg-primary"
+                  className={`flex items-center justify-between  h-16 rounded-md px-4 cursor-pointer hover:bg-gray-700 transition ${
+                    type === "voice" ? " bg-indigo-500" : "bg-secondary"
                   }`}
                 >
                   <div className="flex items-center gap-4">
@@ -139,8 +139,8 @@ export function CreateChannel() {
                 </label>
 
                 <label
-                  className={`flex items-center justify-between  h-16 rounded-md px-4 cursor-pointer hover:bg-secondary transition ${
-                    type === "board" ? "bg-secondary/90" : "bg-primary"
+                  className={`flex items-center justify-between  h-16 rounded-md px-4 cursor-pointer hover:bg-gray-700 transition ${
+                    type === "board" ? " bg-indigo-500" : "bg-secondary"
                   }`}
                 >
                   <div className="flex items-center gap-4">
