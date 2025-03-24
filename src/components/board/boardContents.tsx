a45c28789a6a7a688dbb4b65df3cdfe39a549208
zedworkspace <shanoof.001@gmail.com>
2025-03-24 09:42:22 +0530

    feat:add pop over in list

--- src/components/board/boardContents.tsx
+++ src/components/board/boardContents.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import BoardList from "./boardList";
 import AddList from "./addList";
 import { IList } from "@/interface/listInterface";
-import {
-  horizontalListSortingStrategy,
-  SortableContext,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
 
 type Props = {
   lists?: IList[];
@@ -14,20 +9,12 @@ type Props = {
 };
 
 export default function BoardContents({ lists, boardId }: Props) {
-  const items = lists?.map((item) => ({ ...item, id: item._id }));
   return (
     <div className="flex-1 w-full  overflow-auto scrollbar-hide h-full">
       <div className="min-w-max flex h-full">
-        {/* {items && (
-          <SortableContext
-            items={items}
-            strategy={horizontalListSortingStrategy}
-          > */}
-            {lists?.map((list: IList, index: number) => (
-              <BoardList list={list} index={index} key={list._id} />
-            ))}
-          {/* </SortableContext>
-        )} */}
+        {lists?.map((list: IList, index: number) => (
+          <BoardList list={list} index={index} key={list._id} />
+        ))}
         <AddList boardId={boardId} />
       </div>
     </div>
